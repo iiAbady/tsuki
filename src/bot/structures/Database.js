56d8e8260e041f800579ba6e24abadacb3625998
Abady <gamersspeaks@gmail.com>
2019-02-24 14:58:26 +0300

    chore: .sync()

--- src/bot/structures/Database.js
+++ src/bot/structures/Database.js
@@ -9,4 +9,7 @@ const sequelize = new Sequelize(process.env.DB, {
 sequelize.import(join(__dirname, '..', 'models', 'settings'));
 sequelize.import(join(__dirname, '..', 'models', 'playlists'));
 
+
+sequelize.sync(); 
+
 module.exports = sequelize;
